d4937d2a4116c84a7ec2a91c60ebc0e2f1d23cdc
maurenskie <126791873+maurenskie@users.noreply.github.com>
2023-05-16 21:35:50 +0200

    Add files via upload

---
+++ IWA11 Challenge/Scripts.js
@@ -0,0 +1,34 @@
+// script.js
+
+// Order 1
+const order1 = {
+    biscuits: 5,
+    donuts: 3,
+    pancakes: 2,
+    status: "Delivered"
+  };
+  
+  // Order 2
+  const order2 = {
+    biscuits: 10,
+    donuts: 6,
+    pancakes: 4,
+    status: "Pending"
+  };
+  
+  // Order 3
+  const order3 = {
+    biscuits: 8,
+    donuts: 2,
+    pancakes: 3,
+    status: "Pending"
+  };
+  
+  // Print order details
+  console.log("Order 1:");
+  console.log(order1);
+  console.log("Order 2:");
+  console.log(order2);
+  console.log("Order 3:");
+  console.log(order3);
+  
